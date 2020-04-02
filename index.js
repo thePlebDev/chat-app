4c817f3769171f44b04f5e32e00c2eed8dba921b
Tristan Elliott <tristanelliott7@gmail.com>
2020-04-02 14:13:00 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+  console.log('responding to a request');
+  var url = req.url;
+
+  var fileName = 'index.html';
+  if (url.length > 1){
+    fileName = url.substring(1)
+  }
+  console.log(fileName)
+
+  fs.readFile('app/index.html', function (err,data){
+    res.end(data)
+  })
+});
+
+server.listen(3000);
+// telling the server to listen on port 3000(this is called binding the port)
